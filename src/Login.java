be41f861ddf75b4e7fd6dd1b2d335b284d534df4
CodeGod101 <32391406+RamnathKumar98@users.noreply.github.com>
2018-11-08 17:12:34 +0530

    Final Template. Follow same template

--- src/Login.java
+++ src/Login.java
@@ -28,7 +28,7 @@ public class Login extends JFrame implements ActionListener {
     // main class
     public static void main(String[] args)
     {
-        // create a new frame to store text field and button
+        // create a new frame to stor text field and button
         frmLogin = new JFrame();
         frmLogin.setTitle("Hotel Booking");
         //frmLogin.setResizable(false);
@@ -43,47 +43,63 @@ public class Login extends JFrame implements ActionListener {
         // create a label to display text
         lblNewLabel = new JLabel("Username:");
         lblNewLabel.setForeground(new Color(255,250,250));
-        lblNewLabel.setBounds(83,34,111,27);
-        lblNewLabel.setFont(new Font("Tahoma",Font.BOLD,17));
+        //lblNewLabel.setBounds(83,34,111,27);
+        lblNewLabel.setBounds(166,68,200,50);
+        lblNewLabel.setFont(new Font("Tahoma",Font.BOLD,28));
 
         passwordField = new JPasswordField();
-        passwordField.setBounds(192,92,143,20);
+        //passwordField.setBounds(192,92,143,20);
+        passwordField.setBounds(384,172,300,40);
+        passwordField.setFont(new Font("Tahoma",Font.PLAIN,28));
+
         lblPassword = new JLabel("Password:");
         lblPassword.setForeground(new Color(255,250,250));
-        lblPassword.setBounds(83,86,111,27);
-        lblPassword.setFont(new Font("Tahoma",Font.BOLD,17));
+        //lblPassword.setBounds(83,86,111,27);
+        lblPassword.setBounds(166,172,200,50);
+        lblPassword.setFont(new Font("Tahoma",Font.BOLD,28));
 
         pswdIncorrect = new JLabel("");
-        pswdIncorrect.setBounds(141,133,175,14);
+        //pswdIncorrect.setBounds(141,133,175,14);
+        pswdIncorrect.setBounds(282,266,200,50);
+        pswdIncorrect.setForeground(new Color(255,0,0));
+        pswdIncorrect.setFont(new Font("Tahoma",Font.BOLD,28));
+
 
         label =  new JLabel("LOGIN",SwingConstants.LEFT);
         label.setForeground(new Color(255,250,250));
-        label.setBounds(10,0,71,46);
-        label.setFont(new Font("Tahoma",Font.BOLD,20));
+        label.setBounds(10,2,200,46);
+        label.setFont(new Font("Tahoma",Font.BOLD,30));
 
         panel = new  JPanel();
         //panel.setBounds(0,0,frmLogin.getContentPane().getSize().width,46);
-        panel.setBounds(0,0,900,46);
+        panel.setBounds(0,0,900,60);
         panel.setBackground(new Color(255,165,0));
         frmLogin.getContentPane().add(panel);
         panel.setLayout(null);
 
         panel_1 = new JPanel();
-        panel_1.setBounds(0,46,900,554);
+        panel_1.setBounds(0,60,900,540);
         panel_1.setBackground(new Color(112,128,144));
         frmLogin.getContentPane().add(panel_1);
         panel_1.setLayout(null);
         // create a new button
+
         btnSignIn = new JButton("Sign In");
-        btnSignIn.setBackground(new Color(153,50,204));
-        btnSignIn.setBounds(85,158,89,23);
+        btnSignIn.setBackground(new Color(255,255,255));
+        btnSignIn.setFont(new Font("Tahoma",Font.BOLD,30));
+        //btnSignIn.setBounds(85,158,89,23);
+        btnSignIn.setBounds(170,316,200,50);
 
         btnSignUp = new JButton("Sign Up");
-        btnSignUp.setBackground(new Color(153,50,204));
-        btnSignUp.setBounds(227,158,89,23);
+        btnSignUp.setBackground(new Color(255,255,255));
+        btnSignUp.setFont(new Font("Tahoma",Font.BOLD,30));
+        //btnSignUp.setBounds(227,158,89,23);
+        btnSignUp.setBounds(454,316,200,50);
 
-        textField = new JTextField(10);
-        textField.setBounds(192,40,143,20);
+        textField = new JTextField();
+        textField.setFont(new Font("Tahoma",Font.PLAIN,28));
+        //textField.setBounds(192,40,143,20);
+        textField.setBounds(384,68,300,40);
 
         // create a object of the text class
         Login te = new Login();
@@ -110,12 +126,18 @@ public class Login extends JFrame implements ActionListener {
         //f.show();
     }
 
-    // if the button is pressed
+    // if the vutton is pressed
     public void actionPerformed(ActionEvent e)
     {
         String s = e.getActionCommand();
         if (s.equals("Sign In")) {
-            pswdIncorrect.setText("Sign In");//Go Backend and Initiate new Frame
+            pswdIncorrect.setText("Sign In");
+            /*
+            if (validateUser.validate(textField.getText(), passwordField.getText())
+            {
+              // Go to Home Frame
+            }
+            */
         }
         if (s.equals("Sign Up"))
         {
--- src/Register.java
+++ src/Register.java
@@ -6,7 +6,9 @@ import com.toedter.calendar.JDateChooser;
 
 public class Register extends JFrame implements ActionListener {
     // JTextField
-    static JTextField textField;
+    static JTextField fullName;
+    static JTextField email;
+    static JTextField username;
     // JFrame
     static JPanel contentPane;
     static String dob;
@@ -16,7 +18,8 @@ public class Register extends JFrame implements ActionListener {
     // JButton
     static JDateChooser dp;
 
-    static JButton btnSignIn;
+    static JTextArea tx;
+
     static JButton btnSignUp;
     static JPanel panel;
 
@@ -24,7 +27,11 @@ public class Register extends JFrame implements ActionListener {
 
     // label to diaplay text
     static JLabel label;
+    static JLabel lbDob;
+    static JLabel lbladd;
     static JLabel lblPassword;
+    static JLabel lbluse;
+    static JLabel lblemail;
     static JLabel lblNewLabel;
     static JLabel pswdIncorrect;
 
@@ -34,9 +41,9 @@ public class Register extends JFrame implements ActionListener {
     }
 
     // main class
-    public static void main()
+    public static void main(String[] args)
     {
-        // create a new frame to store text field and button
+        // create a new frame to stor text field and button
         frmLogin = new JFrame();
         frmLogin.setTitle("Hotel Booking");
         //frmLogin.setResizable(false);
@@ -44,55 +51,103 @@ public class Register extends JFrame implements ActionListener {
 
         //frmLogin.pack(); // Important line!!!
         //frmLogin.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frmLogin.setVisible(true);
 
         //frmLogin.setUndecorated(true);
         frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frmLogin.getContentPane().setLayout(null);
         // create a label to display text
-        lblNewLabel = new JLabel("Username:");
+        lblNewLabel = new JLabel("Full Name:");
         lblNewLabel.setForeground(new Color(255,250,250));
-        lblNewLabel.setBounds(83,34,111,27);
-        lblNewLabel.setFont(new Font("Tahoma",Font.BOLD,17));
+        //lblNewLabel.setBounds(83,34,111,27);
+        lblNewLabel.setBounds(166,20,200,50);
+        lblNewLabel.setFont(new Font("Tahoma",Font.BOLD,28));
+
+        lbDob = new JLabel("DOB:");
+        lbDob.setForeground(new Color(255,250,250));
+        //lblNewLabel.setBounds(83,34,111,27);
+        lbDob.setBounds(166,70,200,50);
+        lbDob.setFont(new Font("Tahoma",Font.BOLD,28));
 
         passwordField = new JPasswordField();
-        passwordField.setBounds(192,92,143,20);
-        lblPassword = new JLabel("Password:");
-        lblPassword.setForeground(new Color(255,250,250));
-        lblPassword.setBounds(83,86,111,27);
-        lblPassword.setFont(new Font("Tahoma",Font.BOLD,17));
+        //passwordField.setBounds(192,92,143,20);
+        passwordField.setBounds(384,330,300,40);
+        passwordField.setFont(new Font("Tahoma",Font.BOLD,28));
+
+        lbladd = new JLabel("Address:");
+        lbladd.setForeground(new Color(255,250,250));
+        //lblPassword.setBounds(83,86,111,27);
+        lbladd.setBounds(166,120,200,50);
+        lbladd.setFont(new Font("Tahoma",Font.BOLD,28));
 
         pswdIncorrect = new JLabel("");
-        pswdIncorrect.setBounds(141,133,175,14);
+        //pswdIncorrect.setBounds(141,133,175,14);
+        pswdIncorrect.setBounds(282,266,200,50);
+        pswdIncorrect.setForeground(new Color(255,0,0));
+        pswdIncorrect.setFont(new Font("Tahoma",Font.BOLD,28));
 
-        label =  new JLabel("LOGIN",SwingConstants.LEFT);
+
+        label =  new JLabel("REGISTER",SwingConstants.LEFT);
         label.setForeground(new Color(255,250,250));
-        label.setBounds(10,0,71,46);
-        label.setFont(new Font("Tahoma",Font.BOLD,20));
+        label.setBounds(10,2,200,46);
+        label.setFont(new Font("Tahoma",Font.BOLD,30));
 
         panel = new  JPanel();
         //panel.setBounds(0,0,frmLogin.getContentPane().getSize().width,46);
-        panel.setBounds(0,0,900,46);
+        panel.setBounds(0,0,900,60);
         panel.setBackground(new Color(255,165,0));
         frmLogin.getContentPane().add(panel);
         panel.setLayout(null);
 
         panel_1 = new JPanel();
-        panel_1.setBounds(0,46,900,554);
+        panel_1.setBounds(0,60,900,540);
         panel_1.setBackground(new Color(112,128,144));
         frmLogin.getContentPane().add(panel_1);
         panel_1.setLayout(null);
         // create a new button
-        btnSignIn = new JButton("Sign In");
-        btnSignIn.setBackground(new Color(153,50,204));
-        btnSignIn.setBounds(85,158,89,23);
 
         btnSignUp = new JButton("Sign Up");
-        btnSignUp.setBackground(new Color(153,50,204));
-        btnSignUp.setBounds(227,158,89,23);
+        btnSignUp.setBackground(new Color(255,255,255));
+        btnSignUp.setFont(new Font("Tahoma",Font.BOLD,30));
+        //btnSignUp.setBounds(227,158,89,23);
+        btnSignUp.setBounds(454,380,200,50);
 
-        textField = new JTextField(10);
-        textField.setBounds(192,40,143,20);
+        fullName = new JTextField();
+        fullName.setFont(new Font("Tahoma",Font.PLAIN,28));
+        //textField.setBounds(192,40,143,20);
+        fullName.setBounds(384,20,300,40);
+
+        tx = new JTextArea();
+        tx.setFont(new Font("Tahoma",Font.PLAIN,24));
+        //textField.setBounds(192,40,143,20);
+        tx.setBounds(384,120,300,100);
+
+        lblemail = new JLabel("Email:");
+        lblemail.setForeground(new Color(255,250,250));
+        //lblNewLabel.setBounds(83,34,111,27);
+        lblemail.setBounds(166,230,200,50);
+        lblemail.setFont(new Font("Tahoma",Font.BOLD,28));
+
+        email = new JTextField();
+        email.setFont(new Font("Tahoma",Font.PLAIN,28));
+        //textField.setBounds(192,40,143,20);
+        email.setBounds(384,230,300,40);
+
+        lbluse = new JLabel("Username:");
+        lbluse.setForeground(new Color(255,250,250));
+        //lblNewLabel.setBounds(83,34,111,27);
+        lbluse.setBounds(166,280,200,50);
+        lbluse.setFont(new Font("Tahoma",Font.BOLD,28));
+
+        username = new JTextField();
+        username.setFont(new Font("Tahoma",Font.PLAIN,28));
+        //textField.setBounds(192,40,143,20);
+        username.setBounds(384,280,300,40);
+
+        lblPassword = new JLabel("Password:");
+        lblPassword.setForeground(new Color(255,250,250));
+        //lblPassword.setBounds(83,86,111,27);
+        lblPassword.setBounds(166,330,200,50);
+        lblPassword.setFont(new Font("Tahoma",Font.BOLD,28));
 
         contentPane = new JPanel();
     		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -101,14 +156,13 @@ public class Register extends JFrame implements ActionListener {
         dp = new JDateChooser();
 
         dp.setDateFormatString("yyyy-MM-dd");
-		    dp.setBounds(135, 32, 167, 20);
+		    dp.setBounds(384, 70, 300, 40);
+
 		    panel_1.add(dp);
 
     		// create a object of the text class
         Register te = new Register();
 
-        // addActionListener to button
-        btnSignIn.addActionListener(te);
         btnSignUp.addActionListener(te);
         dp.getCalendarButton().addActionListener(te);
 
@@ -117,11 +171,17 @@ public class Register extends JFrame implements ActionListener {
 
         panel_1.add(passwordField);
         panel_1.add(lblNewLabel);
-        panel_1.add(btnSignIn);
         panel_1.add(btnSignUp);
-        panel_1.add(textField);
+        panel_1.add(fullName);
         panel_1.add(lblPassword);
         panel_1.add(pswdIncorrect);
+        panel_1.add(lbDob);
+        panel_1.add(tx);
+        panel_1.add(lbladd);
+        panel_1.add(lblemail);
+        panel_1.add(email);
+        panel_1.add(lbluse);
+        panel_1.add(username);
 
         // set the size of frame
 
@@ -129,18 +189,14 @@ public class Register extends JFrame implements ActionListener {
         //f.show();
     }
 
-    // if the button is pressed
+    // if the vutton is pressed
     public void actionPerformed(ActionEvent e)
     {
         String s = e.getActionCommand();
-        if (s.equals("Sign In")) {
+        if (s.equals("Sign Up")) {
             pswdIncorrect.setText("Sign In");
             dob = ((JTextField)(dp.getDateEditor().getUiComponent())).getText();//Go Backend and Initiate new Frame
             System.out.println(dob);
-        }
-        if (s.equals("Sign Up"))
-        {
-            Register.main();
         }
     }
 
